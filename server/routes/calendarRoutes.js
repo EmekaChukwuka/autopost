09406076e055a51be4ae6b3020410836df9802e4
Emeka Chukwuka <emekachukwuka63@gmail.com>
2025-10-31 18:51:22 +0100

    Added calendar features & fixed a bug 2

--- server/routes/calendarRoutes.js
+++ server/routes/calendarRoutes.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import { generateCalendar, getCalendar, deleteCalendar } from '../controllers/calendarController.js';
 import { authenticateToken } from '../controllers/regisAuthController.js';
+import  protect  from '../middleware/regisAuthMiddleware.js';
 
 const router = express.Router();
 
-router.post('/generate',authenticateToken, generateCalendar);
-router.post('/get', authenticateToken, getCalendar);
+router.post('/generate',protect, generateCalendar);
+router.post('/get', protect, getCalendar);
 router.post('/delete', deleteCalendar);
 
 export default router;
