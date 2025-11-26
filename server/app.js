887fafdf5b5539f9d11d56fbc9234a59f6973ce8
Emeka Chukwuka <emekachukwuka63@gmail.com>
2025-11-26 19:26:34 +0100

    New update

--- server/app.js
+++ server/app.js
@@ -152,6 +152,23 @@ app.get("/payment-verification", async (req, res) => {
   );
 });
 
+const url = `https://autopost-backend-hbck.onrender.com/`;
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 //
 // 🧾 Trial Info Endpoint
 //
