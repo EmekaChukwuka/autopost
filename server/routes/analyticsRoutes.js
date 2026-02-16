d46db46c05d5d66d5b92504f2935635f28ea43af
Emeka Chukwuka <emekachukwuka63@gmail.com>
2026-02-16 19:57:28 +0100

    Update analyticsRoutes.js

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import PostAnalytics from "../models/PostAnalytics.js";
-import { authenticateToken } from "../middleware/auth.js";
 
 const analyticsRouter = express.Router();
 
