e4bb849760e75a0c0cf05b923d33bb221850c5bd
Emeka Chukwuka <emekachukwuka63@gmail.com>
2026-02-21 13:29:38 +0100

    Update ScheduledPost.js

--- server/models/ScheduledPost.js
+++ server/models/ScheduledPost.js
@@ -9,6 +9,9 @@ const scheduledPostSchema = new mongoose.Schema({
   status: { type: String, enum: ["pending", "posted", "failed"], default: "pending" },
 
   image_required: { type: Boolean, default: false },
+
+image_url : { type: String, default: null},
+
   image_status: {
     type: String,
     enum: ["pending", "attached", "failed"],
